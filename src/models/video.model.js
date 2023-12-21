01416f452ced864312c79fe2f37c7c59b30dab06
Vikas singh varma <Vikashverma3001@gmail.com>
2023-12-21 20:17:28 +0530

    into mongoose aggregate

--- src/models/video.model.js
+++ src/models/video.model.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
 const videoSchema = new mongoose.Schema(
     {
@@ -37,4 +38,5 @@ const videoSchema = new mongoose.Schema(
     },{timestamps: true}
 )
 
+videoSchema.plugin(mongooseAggregatePaginate)
 export const Video = mongoose.model("Video", videoSchema)
