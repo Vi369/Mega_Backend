cfd6dba80d0d88be8cfd337ae82814bb4ec50a53
Vikas singh varma <Vikashverma3001@gmail.com>
2023-12-26 19:06:53 +0530

    login and logout route create

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import { loginUser, logoutUser, registerUser } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 const router = Router()
 
 router.route('/register').post(
@@ -15,6 +16,11 @@ router.route('/register').post(
         }
     ]),
     registerUser
-    )
+)
+
+router.route('/login').post(loginUser)
+
+//secure route
+router.route('/logout').post(verifyJWT, logoutUser)
 
 export default router ;
