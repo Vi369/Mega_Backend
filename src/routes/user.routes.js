2576be4261366375642537948e7862ee80ec1bb2
Vikas singh varma <Vikashverma3001@gmail.com>
2023-12-30 18:45:27 +0530

    set changepassword, getuser,updatedetails , avatar,coverimage, related and getchannelprofile , watchhistory route endpoints

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,16 @@
 import { Router } from "express";
-import { loginUser, logoutUser, refreshAccessToken, registerUser } from "../controllers/user.controller.js";
+import { 
+    loginUser, 
+    logoutUser, 
+    refreshAccessToken, 
+    registerUser,
+    changeCurrentPassword,
+    getCurrentUser,
+    updateAccountDetails,
+    updateUserAvatar,
+    updateUserCoverImage,
+    getUserChannelProfile,
+    getWatchHistory} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 const router = Router()
@@ -22,6 +33,16 @@ router.route('/login').post(loginUser)
 
 //secure route
 router.route('/logout').post(verifyJWT, logoutUser)
-
 router.route('/refresh-token').post(refreshAccessToken)
+router.route('/change-password').post(verifyJWT,changeCurrentPassword)
+router.route('/current-user-details').get(verifyJWT, getCurrentUser)
+router.route('/update-account-details').patch(verifyJWT,updateAccountDetails)
+router.route('/update-profileimage').patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
+router.route('/update-coverimage').patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage)
+//params se data 
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile)
+router.route('/watchhistory').get(verifyJWT, getWatchHistory)
+
+
+
 export default router ;
