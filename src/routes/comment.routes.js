75e87da9cbd41241a5da27e8436bcdb42b844726
Vikas singh varma <Vikashverma3001@gmail.com>
2024-01-10 14:42:52 +0530

    comment routes

---
+++ src/routes/comment.routes.js
@@ -0,0 +1,26 @@
+import { Router } from "express";
+import {
+    addCommentToVideo,
+    addCommentToTweet,
+    updateCommentToVideo,
+    updateCommentToTweet,
+    deleteCommentToVideo,
+    deleteCommentToTweet,
+    getVideoComments,
+    getTweetComments
+} from '../controllers/comment.controller.js'
+
+import { verifyJWT } from '../middlewares/auth.middleware.js'
+
+
+const router = Router()
+
+router.use(verifyJWT);
+
+router.route("/:videoId").get(getVideoComments).post(addCommentToVideo);
+router.route("/:tweetId").get(getTweetComments).post(addCommentToTweet);
+
+router.route("/video/c/:commentId").delete(deleteCommentToVideo).patch(updateCommentToVideo);
+router.route("/tweet/c/:commentId").delete(deleteCommentToTweet).patch(updateCommentToTweet);
+
+export default router
